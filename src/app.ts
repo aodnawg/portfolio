e014e2bba5882328b1040af0198e980d73a92467
aodnawg <mortuary.12@gmail.com>
2020-05-31 19:35:33 +0900

    post effect

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 import * as THREE from "three";
-import { Vector3 } from "three";
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
+// import { GlitchPass } from "three/examples/jsm/postprocessing/GlitchPass.js";
+import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass";
+import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass.js";
+import { FXAAShader } from "three/examples/jsm/shaders/FXAAShader.js";
 
 let scene: THREE.Scene;
 let camera: THREE.Camera, width, height;
@@ -7,6 +12,7 @@ let rainDropVelocitys: THREE.Vector3[];
 let rainGeo: THREE.Geometry;
 let renderer: THREE.WebGLRenderer;
 let europa: THREE.Mesh | undefined;
+let composer: EffectComposer;
 
 const init = () => {
   width = window.innerWidth;
@@ -18,13 +24,13 @@ const init = () => {
   camera.position.y = -10;
   camera.position.z = 10;
 
-  camera.lookAt(new Vector3(0, 0, 0));
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
 
   const ambient = new THREE.AmbientLight();
   scene.add(ambient);
 
-  const directionalLight = new THREE.DirectionalLight(0xeeeeff);
-  directionalLight.position.set(0, 1, 0);
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.4);
+  directionalLight.position.set(0, -1, 0);
   scene.add(directionalLight);
 
   renderer = new THREE.WebGLRenderer();
@@ -37,19 +43,21 @@ const init = () => {
   rainDropVelocitys = [];
   for (let i = 0; i <= 10000; i++) {
     const rainDrop = new THREE.Vector3(
-      Math.random() * 400 - 200,
+      Math.random() * 100 - 50,
       Math.random() * 500 - 250,
-      Math.random() * 400 - 200
+      Math.random() * 100 - 50
     );
-    rainDropVelocitys.push(new THREE.Vector3(0, -Math.random(), 0));
+    rainDropVelocitys.push(
+      new THREE.Vector3(0, -(0.2 + Math.random()) * 0.3, 0)
+    );
     rainGeo.vertices.push(rainDrop);
   }
 
   const rainMaterial = new THREE.PointsMaterial({
     color: 0x000000,
-    size: 0.3,
+    size: 0.1,
     transparent: true,
-    opacity: 0.3,
+    opacity: 0.5,
   });
 
   const rain = new THREE.Points(rainGeo, rainMaterial);
@@ -68,7 +76,22 @@ const init = () => {
   const container = document.body;
   container.appendChild(renderer.domElement);
 
-  renderer.render(scene, camera);
+  // post process
+  composer = new EffectComposer(renderer);
+  const renderPass = new RenderPass(scene, camera);
+  // const glitchPass = new GlitchPass(1000);
+  const fxaaPass = new ShaderPass(FXAAShader);
+  const bloomPass = new UnrealBloomPass(
+    new THREE.Vector2(window.innerWidth, window.innerHeight),
+    0.1,
+    0.1,
+    0.9
+  );
+  composer.addPass(renderPass);
+  composer.addPass(bloomPass);
+  composer.addPass(fxaaPass);
+
+  composer.render();
 };
 
 const animate = () => {
@@ -82,7 +105,7 @@ const animate = () => {
   }
   rainGeo.verticesNeedUpdate = true;
 
-  renderer.render(scene, camera);
+  composer.render();
 
   if (europa) {
     europa.rotation.y += 0.001;
---
+++ src/postprocessing.d.ts
@@ -0,0 +1,7 @@
+declare module "postprocessing" {
+  const BloomEffect: any;
+  const EffectComposer: any;
+  const EffectPass: any;
+  const RenderPass: any;
+  const AntialiasingPass: any;
+}
