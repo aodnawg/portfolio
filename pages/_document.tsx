45be676eb33ea40ed9c0e21aaf2a0edcbff5caea
aodnawg <mortuary.12@gmail.com>
2020-06-01 00:59:27 +0900

    ogp

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,6 +18,12 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,900;1,100&display=swap"
             rel="stylesheet"
           ></link>
+          <meta property="og:url" content="https://aodnawg.space" />
+          <meta property="og:title" content="aodnawg" />
+          <meta property="og:image" content="https://aodnawg.space/ogp.jpg" />
+          <meta property="og:description" content="aodnawg" />
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:site" content="@aodnawg" />
         </Head>
         <body>
           <Main />
